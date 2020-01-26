05c2e86c89725df2b667a2a0dd908ec61226ca19
Joe Rourke <josephrourke1@gmail.com>
2020-01-26 18:51:30 +0000

    Error fix: the UUID field was not properly configured

--- src/db/UserTable.py
+++ src/db/UserTable.py
@@ -1,15 +1,17 @@
 from src.db import db
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 class UserTable(db.Model):
     __tablename__ = "usertable"
-    userid = db.Column(db.String(128), primary_key=True)
+    userid = db.Column(UUID(as_uuid=True), primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(355), unique=True, nullable=False)
     lastlogin = db.Column(db.DateTime)
 
     def __init__(self, data):
-        self.userid = "@7bj?U!z5+QHD@Y8n2g@uwTbYMMD3?!^G#pCY*?t!F5mMzxvH?BG3dg%hejVAx=58X6xMt6PdCbc+7@Sg34?^zM-eX*Gz+Tgh9n_NaVL%=eV2s?Qfy3u&yVWvkw&=KB&" # Place in once utils key hash function implemented
+        self.userid = uuid.UUID(data["userid"]).int  # Place in once utils key hash function implemented
         self.username = data["username"]
         self.password = data["password"]
         self.email = data["email"]
