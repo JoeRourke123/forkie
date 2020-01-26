c187eaf4b112d9cc0c31d8ba55599b79b456a2ce
Joe Rourke <josephrourke1@gmail.com>
2020-01-26 16:04:11 +0000

    Added imports and global object definitions

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import sys
+import json
+from flask_heroku import Heroku
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+heroku = Heroku(app)
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-	return "<h1>Hello!</h1>"
+    return "<h1>Hello!</h1>"
+
 
 if __name__ == "main":
-	app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000)
