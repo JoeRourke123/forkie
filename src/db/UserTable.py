71495091fd1d42e61934a60913f3969a938fdc07
Joe Rourke <josephrourke1@gmail.com>
2020-01-26 19:03:17 +0000

    Error fix: Postgres wanted the UUID hex as a string not an int

--- src/db/UserTable.py
+++ src/db/UserTable.py
@@ -1,15 +1,15 @@
 from src.db import db
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import text
 import uuid
+
 
 class UserTable(db.Model):
     __tablename__ = "usertable"
-    userid = db.Column(UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()"))
+    userid = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(355), unique=True, nullable=False)
-    lastlogin = db.Column(db.DateTime)
+    lastlogin = db.Column(db.DateTime, server_default=db.func.now())
 
     def __init__(self, data):
         self.username = data["username"]
