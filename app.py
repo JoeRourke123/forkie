7baf59118dde5166f5278b6ac76eb6d08c2c7a56
Joe Rourke <josephrourke1@gmail.com>
2020-01-27 12:33:00 +0000

    Modularised the sign in and sign up process

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, request, render_template, url_for, make_response
-from src.db import db
-from src.db.UserTable import UserTable
-from src.db.GroupTable import GroupTable
-from src.db.FileTable import FileTable
-from src.db.FileVersionTable import FileVersionTable
-from src.utils import result
-
+from flask import Flask, request, render_template, url_for, make_response, redirect
 from flask_heroku import Heroku
 
 from datetime import datetime
 import sys
+import json
+
+from sqlalchemy.exc import IntegrityError
+
+
+from src.db import db
+from src.db.UserTable import UserTable
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -20,56 +21,112 @@ db.init_app(app)
 
 # Routes
 @app.route("/")
-def index():
-    return render_template("index.html")
+def index(msg=None):
+    if request.cookies.get('userid'):
+        return redirect(url_for('dashboard'))
+
+    return render_template("index.html", msg=msg)
 
 
-# APIs
-@app.route("/api/0.1/signup", methods=["POST"])
-def signup():
-    userdata = UserTable.UserTable({            # Define an instance of the UserTable class with the entered data
-        "username": request.form["username"],
-        "email": request.form["email"],
-        "password": request.form["password"],  # Replace with hashed password eventually
-        "lastlogin": datetime.now()
-    })
+@app.route("/dash")
+def dashboard():
+    if not request.cookies.get('userid'):
+        return redirect(url_for('index', msg="Please sign in to see your dashboard"))
 
-    try:
-        db.session.add(userdata)                # Add the newly instantiated object to the DB
-        db.session.commit()                     # Ensure the database transaction properly completes
-    except Exception as e:
-        print("Failed Signup for user... " + request.form["email"])
-        print(e)
-        sys.stdout.flush()                      # Output to Heroku log if an error occurs
+    return render_template("dashboard.html")
 
-        return result(500)                      # Return a 500 error code
 
-    return result(200)                          # If successful, return a 200 code
+# Web Endpoints
+@app.route("/api/web/signin", methods=["POST"])
+def webSignin():
+    req = signin(request.form)
 
+    if req['result'] == 200:
+        resp = make_response(redirect(url_for('dashboard')))
+        resp.set_cookie('userid', req['userid'])
+        return resp
+    elif req['result'] == 400:
+        return redirect(url_for('index', msg="Sorry, we don't recognise that email/password combination"))
+    elif req['result'] == 500:
+        return redirect(url_for('index', msg="It appears an error has occured. Please try again"))
+
+
+@app.route("/api/web/signup", methods=["POST"])
+def webSignup():
+    req = signup(request.form)
+
+    if req['result'] == 200:
+        return redirect(url_for('index', msg="Account created. You can now signin!"))
+    elif req['result'] == 500:
+        return redirect(url_for('index', msg="Sorry, something went wrong. Please try again!"))
+    elif req['result'] == 400:
+        return redirect(url_for('index', msg="That email is already in use, do you want to sign in?"))
+
+
+# CLI Endpoints
+@app.route("/api/cli/signup", methods=["POST"])
+def apiSignup():
+    return json.dumps(signup(request.json))
 
 @app.route("/api/0.1/signin", methods=["POST"])
-def signin():
-    if request.form["email"] or request.form["password"]:       # Server side check for user email and password entry
+def apiSignin():
+    return json.dumps(signin(request.json))
+
+
+# Sign in/up functions
+def signin(data):
+    if data["email"] or data["password"]:       # Server side check for user email and password entry
         try:
-            query = UserTable.query.filter_by(email=request.form['email']).filter_by(password=request.form['password']).first()
+            query = UserTable.query.filter_by(email=data['email']).filter_by(password=data['password']).first()
             # Query the database with the entered email and password combination
 
-            if not query:                   # If no results are returned, the email/password are incorrect
-                return result(400)
+            if not query:                   # If no results are returned, the email/password are incorrect, return forbidden code
+                return {
+                    "result": 400
+                }
 
             query.lastlogin = datetime.now()        # If it is found, update the lastlogin field
             db.session.commit()
 
-            response = make_response(result(200, {      # Make a 200 accepted response containing the user's ID
+            return {
+                "result": 200,
                 "userid": str(query.userid)
-            }))
-            response.set_cookie('userid', str(query.userid))    # Set the ID as a cookie also
-
-            return response
+            }
         except Exception as e:
             print(e)
             sys.stdout.flush()
-            return result(500)
+            return {
+                "result": 500
+            }
+
+
+def signup(data):
+    userdata = UserTable.UserTable({  # Define an instance of the UserTable class with the entered data
+        "username": data["username"],
+        "email": data["email"],
+        "password": data["password"],  # Replace with hashed password eventually
+        "lastlogin": datetime.now()
+    })
+
+    try:
+        db.session.add(userdata)  # Add the newly instantiated object to the DB
+        db.session.commit()  # Ensure the database transaction properly completes
+    except IntegrityError as e:  # Thrown if the user attempts to use an email that already exists in the table
+        return {
+            "result": 400,
+        }
+    except Exception as e:
+        print("Failed Signup for user... " + data["email"])
+        print(e)
+        sys.stdout.flush()  # Output to Heroku log if an error occurs
+
+        return {
+            "result": 500
+        }  # Return a 500 error code
+
+    return {
+        "result": 200
+    }  # If successful, return a 200 code
 
 
 if __name__ == "main":
