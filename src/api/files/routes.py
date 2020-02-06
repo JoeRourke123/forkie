885bb18b14826542cf40e806c0a58d87eb0220da
Jakab Zeller <jakabzeller0@gmail.com>
2020-02-06 21:42:36 +0000

    file_query API endpoint init
    Don't know if this works tbh ¯\_(ツ)_/¯. I think that's how multi-queries work in flask_sqlalchemy. If it's not please tell me

---
+++ src/api/files/routes.py
@@ -0,0 +1,93 @@
+from flask import current_app as app
+from flask import render_template, Blueprint, request, make_response, redirect, url_for
+
+from src.db.FileTable import FileTable
+from src.db.FileGroupTable import FileGroupTable
+from src.db.FileVersionTable import FileVersionTable
+from src.utils import hashPassword
+from src.db import db
+
+from datetime import datetime
+import json
+from jsonschema import validate, ValidationError
+# from sqlalchemy.orm import sessionmaker
+# Session = sessionmaker(bind=engine)
+# session = Session()
+
+signinBP = Blueprint('file_query', __name__, template_folder='../../templates', static_folder='../../static', url_prefix='/api')
+query_schema = {
+    "type": "object",
+    "properties": {
+        "filename": {"type": "string"},
+        "versionid": {"type": "string"},
+        "fileid": {"type": "number"},
+        "extension": {"type": "string"},
+        "groupid": {"type": "number"},
+        "versionhash": {"type": "number"},
+        "first": {"type": "boolean"}
+    }
+}
+
+""" Types of ways to query files
+    - by all (when nothing is inside JSON query)
+    - by filename (file table)
+    - by version ID (file version table)
+    - by extension (file version table)
+    - by versionhash (file version table)
+    - by groupid (file group table)
+    - by file ID (all file tables)
+    - by first query result (boolean)
+"""
+
+@signinBP.route("/file_query", methods=["POST"])
+def file_query():
+    isBrowser = bool(request.form is not None)
+    data = request.form if isBrowser else request.json
+    
+    # Validate data
+    try:
+        validate(instance=data, schema=query_schema)
+        try:
+            userid = request.cookies.get('userid')
+            # query = session.query(FileTable)
+            # all_rows = query.all()
+            query = FileTable.query.order_by(FileTable.filename)
+            if len(data.keys()) > 0:
+                if "filename" in data:
+                    query = query.filter(FileTable.filename == data["filename"])
+                if "fileid" in data:
+                    query = query.filter(FileTable.fileid == data["fileid"])
+                if "versionid" in data:
+                    query = query.filter(FileTable.fileid == FileVersionTable.fileid, FileVersionTable.versionid == data["versionid"])
+                if "extension" in data:
+                    query = query.filter(FileTable.fileid == FileVersionTable.fileid, FileVersionTable.versionid == data["extension"])
+                if "versionhash" in data:
+                    query = query.filter(FileTable.fileid == FileVersionTable.fileid, FileVersionTable.versionid == data["versionhash"])
+                if "groupid" in data:
+                    query = query.filter(FileTable.fileid == FileGroupTable.fileid, FileGroupTable.groupid == data["groupid"])
+                if "first" in data:
+                    if data["first"]:
+                        query = query.first()
+
+            rs = query.all()
+            resp = make_response(json.dumps({"code": 200, "msg": "Here are the returned rows", "rows": [(dict(row.items())) for row in rs]})))
+            resp.set_cookie("userid", userid)
+            resp.set_cookie("client", "browser" if isBrowser else "cli")
+        except Exception as e:
+            print(e.with_traceback())
+
+            if isBrowser:
+                return redirect(url_for("errors.error", code=500, url="file_query.file_query"))
+            else:
+                return json.dumps({
+                    "code": 500,
+                    "msg": "Something went wrong when querying files"
+                }), 500
+    except ValidationError:
+        if isBrowser:
+            return redirect(url_for("errors.error", code=400, url="file_query.file_query"))
+        else:
+            return json.dumps({
+                "code": 400,
+                "msg": "There was something wrong with the data you sent, please check and try again"
+            })
