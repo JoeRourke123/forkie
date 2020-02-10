691ede33d0f58cdf0b2ecfc23d16a268214461e8
Jakab Zeller <jakabzeller0@gmail.com>
2020-02-10 16:56:58 +0000

    Debugging user.utils (no. 2)

--- src/api/user/utils.py
+++ src/api/user/utils.py
@@ -15,11 +15,12 @@ def getUserGroupsID(userid: str) -> list:
         - returns: list of groups containing member
     """
     print("Reached getUserGroupsID")
-    return db.session.query(UserGroupTable).query.filter(UserGroupTable.userid == userid).all()
+    return db.session.query(UserGroupTable).filter(UserGroupTable.userid == userid).all()
 
 def getFilesUserCanAccess(userid: str):
     groups = getUserGroupsID(userid)
-    print([group.groupid for group in groups])
+    groupids = [group.groupid for group in groups]
+    print(groupids)
     return FileTable.query.join(FileGroupTable, FileTable.fileid == FileGroupTable.fileid)\
         .add_columns(FileTable.fileid, FileTable.filename, FileGroupTable.groupid)\
         .filter(FileTable.fileid == FileGroupTable.fileid)\
