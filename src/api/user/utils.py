6cd2344d29ddd7a9019e90954760a1002c36401b
Jakab Zeller <jakabzeller0@gmail.com>
2020-02-10 15:43:31 +0000

    Tryed to fix UserTable mapping error

--- src/api/user/utils.py
+++ src/api/user/utils.py
@@ -7,7 +7,7 @@ from src.db.FileGroupTable import FileGroupTable
 
 
 def getUserData(userid):
-    return UserTable.query.filter_by(userid=userid).first()
+    return UserTable.query.filter(UserTable.userid == userid).first()
 
 def getUserGroupsID(userid: str) -> list:
     """ Returns the groupids of the groups that the user (of userid) is a member of
