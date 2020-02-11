7aadb385dc75f798d71387f95f44231005b7e74c
Joe Rourke <josephrourke1@gmail.com>
2020-02-11 20:23:58 +0000

    Fixed UUID generation and linking tables, converted to models

--- src/db/FileVersionTable.py
+++ src/db/FileVersionTable.py
@@ -6,7 +6,7 @@ import uuid
 class FileVersionTable(db.Model):
     __tablename__ = "fileversiontable"
 
-    versionid = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
+    versionid = db.Column(UUID(as_uuid=True), primary_key=True)
     fileid = db.Column(UUID(as_uuid=True), db.ForeignKey('filetable.fileid'), nullable=False)
     extension = db.Column(db.String(16), nullable=False)
     versionhash = db.Column(db.String(8), nullable=False)
@@ -14,7 +14,7 @@ class FileVersionTable(db.Model):
     file = db.relationship(FileTable, foreign_keys=fileid, backref=db.backref('file', lazy='joined'))
 
     def __init__(self, data):
-        self.versionid = "" # hash the time and user attempting to upload the version
+        self.versionid = str(uuid.uuid1())
         self.fileid = data["fileid"]
         self.extension = data["extension"]
         self.versionhash = data["versionhash"]
--- src/db/UserGroupTable.py
+++ src/db/UserGroupTable.py
@@ -1,9 +1,14 @@
 from src.db import db
 from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 
-UserGroupTable = db.Table(
-    'usergrouptable',
-    db.Column('userid', UUID(as_uuid=True), db.ForeignKey('usertable.userid'), primary_key=True),
-    db.Column('groupid', UUID(as_uuid=True), db.ForeignKey('grouptable.groupid'), primary_key=True)
-)
+class UserGroupTable(db.Model):
+    __tablename__ = "usergrouptable"
+
+    userid = db.Column(UUID(as_uuid=True), db.ForeignKey('usertable.userid'), primary_key=True)
+    groupid = db.Column(UUID(as_uuid=True), db.ForeignKey('grouptable.groupid'), primary_key=True)
+
+    def __init__(self, data):
+        self.groupid = data["groupid"]
+        self.userid = data["userid"]
