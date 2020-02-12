d4bf589a495382d1978d2b77187191557bf3a1ab
JoeRourke123 <josephrourke1@gmail.com>
2020-02-12 18:30:33 +0000

    Added boilerplate code for filecreate, backblaze implementation still needed

--- src/api/files/file_create.py
+++ src/api/files/file_create.py
@@ -1,44 +1,87 @@
 from flask import current_app as app
 from flask import render_template, Blueprint, request, make_response, redirect, url_for
 
-from src.api.user.func import getUserGroups
 from src.db.FileTable import FileTable
 from src.db.FileVersionTable import FileVersionTable
 from src.db.FileGroupTable import FileGroupTable
-from src.utils import hashPassword
+from src.db.MetadataTable import MetadataTable
 from src.db import db
 
-from sqlalchemy.exc import IntegrityError
 from datetime import datetime
 import json
 
-### IGNORE THIS FILE I'M NOT FINISHED
-
-### Create JSON SCHEMA FOR CREATING FILES
-
-fCreateBP = Blueprint('signup', __name__, template_folder='../../templates', static_folder='../../static', url_prefix='/api')
+filesBP = Blueprint('files', __name__, template_folder='../../templates', static_folder='../../static', url_prefix='/api/files')
 
 
-@fCreateBP.route("/signup", methods=["POST"])
-def signup():
-    isBrowser = bool(request.form is not None)
-    data = request.form if isBrowser else request.json
+@filesBP.route("/new", methods=["POST"])
+def newFile():
+    isBrowser = "filename" in request.form
+    data = request.form if isBrowser else request.data
     
     # If there is no userid inside the cookie from a cli user then return 401 (unauthorized error)
-    if "userid" not in request.cookies and not isBrowser:
-        return json.dumps({
-            "code": 401,
-            "msg": "Unauthorized. Make sure you sure you have logged in."
+    if "userid" not in request.cookies:
+        if isBrowser:
+            return redirect(url_for('errors.error', code=401))
+        else:
+            return json.dumps({
+                "code": 401,
+                "msg": "You must be signed in to do this",
+            }), 401
+
+
+    if "file" not in request.files:
+        if isBrowser:
+            return redirect(url_for('errors.error', code=406, msg="No files were included in the request"))
+        else:
+            return json.dumps({
+                "code": 406,
+                "msg": "No files were included in the request",
+            }), 406
+
+    upload = request.files["file"]
+
+    try:
+        # DO BACKBLAZE STUFF
+
+        file = FileTable({
+            "filename": upload.filename,
+            "extension": upload.filename.split(".")[-1],
         })
-    
-    # Create data inside filetable
-    filedata = FileTable({
-        "filename": data["username"]
-    })
-    
-    # Create
-    fileversiondata = FileVersionTable({
-        "fileid": data["fileid"],
-        "extension": data["extension"],
-        "versionhash": data["versionhash"]
-    })
+
+        db.session.add(file)
+        db.session.commit()
+
+        filegroup = FileGroupTable({
+            "fileid": str(file.fileid),
+            "groupid": data["groupid"]
+        })
+
+        fileversion = FileVersionTable({
+            "fileid": str(file.fileid),
+            "versionhash": ""
+        })
+
+        db.session.add(fileversion)
+        db.session.add(filegroup)
+        db.session.commit()
+
+        userMetadata = MetadataTable({
+            "versionid": str(fileversion.versionid),
+            "title": "userid",
+            "value": request.cookies.get("userid")
+        })
+
+        timeMetadata = MetadataTable({
+            "versionid": str(fileversion.versionid),
+            "title": "uploaded",
+            "value": str(datetime.now())
+        })
+
+        db.session.add(userMetadata)
+        db.session.add(timeMetadata)
+        db.session.commit()
+
+        # This is so gross, I'll modularise it at some point
+
+    except Exception as e:
+        print(e)
