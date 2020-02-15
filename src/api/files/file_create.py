c4305d06c6a9859d50907c0a346701c555933290
JoeRourke123 <josephrourke1@gmail.com>
2020-02-15 18:15:42 +0000

    Created newFileVersion method to create a new file version

--- src/api/files/file_create.py
+++ src/api/files/file_create.py
@@ -1,8 +1,13 @@
+from traceback import print_exc
+
 from flask import Blueprint, request, redirect, url_for
 
+from src.api.user.utils import getUserData
 from src.db.FileTable import FileTable
 from src.db.FileGroupTable import FileGroupTable
 from src.db import db
+
+from src.api.files.utils import newFileVersion
 
 from datetime import datetime
 import json
@@ -12,7 +17,7 @@ filesBP = Blueprint('files', __name__, template_folder='../../templates', static
 
 @filesBP.route("/new", methods=["POST"])
 def newFile():
-    isBrowser = "filename" in request.form
+    isBrowser = "groupid" in request.form
     data = request.form if isBrowser else request.data
     
     # If there is no userid inside the cookie from a cli user then return 401 (unauthorized error)
@@ -54,7 +59,8 @@ def newFile():
         db.session.add(filegroup)
         db.session.commit()
 
-        # newVersion(file, upload) - function when called will create a new file version for the initial upload
+        return newFileVersion(file, upload, getUserData(request.cookies.get("userid")))
 
     except Exception as e:
-        print(e)
+        print(print_exc())
+        return str(e)
--- src/api/files/utils.py
+++ src/api/files/utils.py
@@ -2,5 +2,37 @@
 """
 import os
 
+from src.api.files.backblaze import B2Interface
+
+from src.db import db
+from src.db.FileVersionTable import FileVersionTable
+
+from src.api.files.backblaze import application_key
+from src.api.files.backblaze import application_key_id
+from src.api.files.backblaze import file_rep_bucket
+
+
 def getFileExtension(filename: str) -> str:
     return os.path.splitext(filename)[1]
+
+
+def newFileVersion(fileData, uploadData, userData):
+    b2 = B2Interface(application_key_id, application_key, file_rep_bucket)
+
+    fileversion = FileVersionTable({
+        "fileid": fileData.fileid,
+        "versionhash": "temp"
+    })
+
+    upload = b2.uploadFile(data=uploadData.read(),
+                  versionid=fileversion.versionid,
+                  filename=uploadData.filename,
+                  fileid=str(fileData.fileid),
+                  extension=fileData.extension)
+
+    fileversion.versionhash = upload.get_content_sha1()
+
+    db.session.add(fileversion)
+    db.session.commit()
+
+    return "File Uploaded"
