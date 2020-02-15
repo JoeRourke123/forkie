662e7e27c2b3d9f8c26e9fef99fcf1059c34a693
JoeRourke123 <josephrourke1@gmail.com>
2020-02-15 14:03:16 +0000

    Fixed removing users from a group

--- src/api/groups/routes.py
+++ src/api/groups/routes.py
@@ -73,16 +73,19 @@ def removeMember():
 
     if request.cookies.get("userid"):
         try:
+            print(data)
             group = GroupTable.query.filter_by(groupid=data["groupid"]).first()
 
-            if not group or (group.groupleaderid is not request.cookies.get("userid") and request.cookies.get("userid") is not data["userid"]):
+            if not group or not (str(group.groupleaderid) is not request.cookies.get("userid") and request.cookies.get("userid") is not data["userid"]):
                 return redirect(url_for('dash', msg="Sorry you are not permitted to complete this action"))
 
-            UserGroupTable.delete().where(and_(userid=data["userid"], groupid=data["groupid"]))
-            UserGroupTable.execute()
+            usergroup = UserGroupTable.query.filter(and_(UserGroupTable.userid==data["userid"], UserGroupTable.groupid==data["groupid"])).first()
+            db.session.delete(usergroup)
+            db.session.commit()
 
             return redirect(url_for('group', id=group.groupid, msg="User has been removed from " + group.groupname))
         except Exception as e:
+            print(print_exc())
             return str(e)
 
             if isBrowser:
@@ -105,7 +108,56 @@ def removeMember():
 
 @groupsBP.route("/new", methods=["POST"])
 def newGroup():
-    isBrowser = "groupName" in request.form
+    isBrowser = "groupname" in request.form
+    data = request.form if isBrowser else request.data
+
+    if request.cookies.get("userid"):
+        try:
+            group = GroupTable({
+                "groupname": data.get("groupname"),
+                "groupleaderid": request.cookies.get("userid")
+            })
+            db.session.add(group)
+            db.session.commit()
+
+            usergroup = UserGroupTable({
+                "groupid": str(group.groupid),
+                "userid": request.cookies.get("userid")
+            })
+            db.session.add(usergroup)
+            db.session.commit()
+        except Exception as e:
+            return str(e)
+
+            if isBrowser:
+                return redirect(url_for('errors.error', code=500, msg=print_exc()))
+            else:
+                return json.dumps({
+                    "code": 500,
+                    "msg": "Something went wrong when creating your group"
+                }), 500
+    else:
+        if isBrowser:
+            return redirect(url_for('errors.error', code=403, msg=print_exc()))
+        else:
+            return json.dumps({
+                "code": 403,
+                "msg": "You must be signed in to do this",
+                "exc": print_exc()
+            }), 403
+
+    if isBrowser:
+        return redirect(url_for('group', id=group.groupid))
+    else:
+        return json.dumps({
+            "code": 200,
+            "msg": "Your group has been created"
+        })
+
+
+@groupsBP.route("/rename", methods=["POST"])
+def renameGroup():
+    isBrowser = "groupid" in request.form
     data = request.form if isBrowser else request.data
 
     if request.cookies.get("userid"):
