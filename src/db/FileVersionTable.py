106548b8a4eec58539c095d6e615c7b6414d04a0
JoeRourke123 <josephrourke1@gmail.com>
2020-02-26 15:53:46 +0000

    Added archived field to the fileversiontable

--- src/db/FileVersionTable.py
+++ src/db/FileVersionTable.py
@@ -10,6 +10,7 @@ class FileVersionTable(db.Model):
     versionid = db.Column(UUID(as_uuid=True), primary_key=True)
     fileid = db.Column(UUID(as_uuid=True), db.ForeignKey('filetable.fileid', ondelete='CASCADE'), nullable=False)
     versionhash = db.Column(db.String(8), nullable=False)
+    archived = db.Column(db.Boolean(), unique=False, nullable=False, default=False)
 
     file = db.relationship(FileTable, foreign_keys=fileid, backref=db.backref('file', lazy='joined', cascade="all, delete-orphan"))
 
@@ -17,3 +18,4 @@ class FileVersionTable(db.Model):
         self.versionid = str(uuid.uuid1())
         self.fileid = data["fileid"]
         self.versionhash = data["versionhash"]
+        self.archived = False
