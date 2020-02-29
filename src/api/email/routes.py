38afd01499b1b7ca486a970274740a18ed78ff6e
JoeRourke123 <josephrourke1@gmail.com>
2020-02-29 14:27:33 +0000

    Commented email package, and included util to email admins in case of an error

--- src/api/email/routes.py
+++ src/api/email/routes.py
@@ -7,6 +7,7 @@ from src.api.user.utils import getUserData
 from src.api.groups.utils import getGroupUsers
 from src.api.email.utils import sendGroupEmail
 
+# Email blueprint to modularise the email related endpoints away from the main app routes
 emailBP = Blueprint('email', __name__,
                     template_folder='../../templates',
                     static_folder='../../static',
@@ -15,25 +16,38 @@ emailBP = Blueprint('email', __name__,
 
 @emailBP.route("/group", methods=["POST"])
 def emailGroup():
-    isBrowser = "groupID" in request.form
-    data = request.form if isBrowser else request.data
+    """ POST Endpoint for sending the users in a group an email.
+        - groupid: the UUID of the group to send the email to
+        - userid: accessed from the user cookie, indicates the sender of the email
+        - subject: the subject header of the email being sent
+        - content: the content to include in the requested email body
+
+        - returns: a success response/redirection if the email sends successfully, or a error response otherwise
+    """
+
+    isBrowser = "groupid" in request.form
+    data = request.form if isBrowser else json.loads(request.data)
 
     if request.cookies.get("userid"):
-        userData = getUserData(request.cookies.get("userid"))
-        groupUsers = getGroupUsers(data["groupid"])
+        userData = getUserData(request.cookies.get("userid"))   # Retrieves dictionary of userData from the util function
 
-        if userData not in groupUsers:
-            return redirect(url_for('errors.error', code=403, msg="You're not in this group so cannot send this message"))
+        if userData not in getGroupUsers(data["groupid"]) and not userData["admin"]:    # If the user isn't in the group
+            if isBrowser:                                                               # and isn't an admin
+                return redirect(url_for("dash", msg="You're not allowed to send emails to this group"))
+            else:
+                return json.dumps({
+                    "code": 401,
+                    "msg": "You don't have permission to send emails to this group"
+                }), 401
 
-        sendGroupEmail(data["groupid"], data, userData)
+        sendGroupEmail(data["groupid"], data, userData)         # User data, groupid, and email data sent to the send email util function
     else:
         if isBrowser:
-            return redirect(url_for('errors.error', code=403, msg=print_exc()))
+            return redirect(url_for("index", code=403, msg="You must be signed in to do this!"))
         else:
             return json.dumps({
                 "code": 403,
                 "msg": "You must be signed in to do this",
-                "exc": print_exc()
             }), 403
 
     if isBrowser:
--- src/api/email/utils.py
+++ src/api/email/utils.py
@@ -2,11 +2,20 @@ import sendgrid
 import os
 from traceback import print_exc
 
-from src.api.groups.utils import getGroupUsers
+from src.api.groups.utils import getGroupUsers, getGroupData
+from src.api.user.utils import getAdmins
 
-def sendGroupEmail(groupID, data, sender):
-    groupUsers = getGroupUsers(groupID)
-    sg = sendgrid.SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+SG_KEY = os.environ.get('SENDGRID_API_KEY')
+
+""" Util function which upon calling, with the correct parameters, sends an email to all members of a group.
+    - groupid: the UUID of the group whose users will receive the email
+    - data: the email data (must include the subject, and content of the email)
+    - sender: the user data dictionary of the email sender/file uploader
+    - returns: void
+"""
+def sendGroupEmail(groupid, data, sender):
+    groupUsers = getGroupUsers(groupid)
+    sg = sendgrid.SendGridAPIClient(SG_KEY)
 
     try:
         for user in groupUsers:
@@ -14,9 +23,38 @@ def sendGroupEmail(groupID, data, sender):
                                  str(user["email"]), str(data["subject"] + " - from " + sender["username"]),
                                  str(data["content"]))
             response = sg.send(mail)
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
+
     except Exception as e:
         print(print_exc())
-        # sendErrorEmail(groupID, data, sender)
+        sendErrorEmail(groupid, data, sender)
+
+""" Error handling function that is triggered upon error sending group email, sends 
+    - groupid: the UUID of the group in which the bulk email was attempted
+    - data: the attempted email data (must include the subject, and content of the email)
+    - sender: the user data dictionary of the attempted email sender/file uploader
+    - returns: void
+"""
+def sendErrorEmail(groupid, data, sender):
+    admins = getAdmins()
+    sg = sendgrid.SendGridAPIClient(SG_KEY)
+
+    groupData = getGroupData(groupid)
+
+    try:
+        for admin in admins:
+            mail = sendgrid.Mail("forkie-error@example.com",
+                                 str(admin["email"]), "Error sending email to group " + groupData["groupname"],
+                                 plain_text_content="""
+                                    There was an error when %s uploaded a file and attempted to send an email to group %s (%s).
+                                    
+                                    Please attend to logs as soon as possible to resolve the issue.
+                                    
+                                    Here was the email content...
+                                    "%s"
+                                    
+                                    Thank you,
+                                    forkie Admin Alert
+                                 """.format([sender["username"], groupData["groupid"], groupData["groupname"], data["content"]]))
+            response = sg.send(mail)
+    except Exception as e:
+        print(print_exc())
