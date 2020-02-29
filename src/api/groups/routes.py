d8b4a576d9500c15335a892b2798cedc242b3dab
JoeRourke123 <josephrourke1@gmail.com>
2020-02-29 20:59:38 +0000

    Commented the group API endpoints

--- src/api/groups/routes.py
+++ src/api/groups/routes.py
@@ -21,6 +21,15 @@ groupsBP = Blueprint('groups', __name__,
 
 @groupsBP.route("/addMember", methods=["POST"])
 def addMember():
+    """ Endpoint to add a user to a specified group
+
+        - groupid: the UUID of the group in which the new member will be added to
+        - userid: the UUID of the user who triggered the group member addition
+        - email: the email address of the user to add to the group
+
+        - returns: a response for the client to interpret based on the success of the operation
+    """
+
     isBrowser = "email" in request.form
     data = request.form if isBrowser else json.loads(request.data)
 
@@ -28,8 +37,8 @@ def addMember():
         try:
             group = GroupTable.query.filter(and_(GroupTable.groupleaderid==request.cookies.get("userid"), GroupTable.groupid==data["groupid"])).first()
 
-            if not group:
-                if isBrowser:
+            if not group:  # If a group where the current user is the leader and the id is the one supplied is not found
+                if isBrowser:   # The user must not have leader permissions and must be redirected
                     return redirect(url_for('dash', msg="Sorry you are not permitted to complete this action"))
                 else:
                     return json.dumps({
@@ -37,12 +46,14 @@ def addMember():
                         "msg": "You don't have permission to complete this action!"
                     }), 401
 
-
+            # Get the user data from the inputted user email
             newUserData = UserTable.query.filter_by(email=data["email"]).first()
 
+            # If there isn't a user with that email
             if not newUserData:
                 return redirect(url_for('group', id=group.groupid, msg="Sorry, we couldn't find that user!"))
 
+            # Otherwise add them to the user-group linking table
             usergroup = UserGroupTable({
                 "groupid": group.groupid,
                 "userid": newUserData.userid
@@ -50,7 +61,7 @@ def addMember():
             db.session.add(usergroup)
             db.session.commit()
 
-
+            # Inform the user that the action was successful
             if isBrowser:
                 return redirect(url_for('group', id=group.groupid, msg=newUserData.username + " has been added to " + group.groupname))
             else:
@@ -60,10 +71,9 @@ def addMember():
                 })
         except Exception as e:
             print(print_exc())
-            return str(e)
 
             if isBrowser:
-                return redirect(url_for('errors.error', code=500, msg=print_exc()))
+                return redirect(url_for("group", id=data["groupid"], msg="Something went wrong when adding member"))
             else:
                 return json.dumps({
                     "code": 500,
@@ -71,7 +81,7 @@ def addMember():
                 }), 500
     else:
         if isBrowser:
-            return redirect(url_for('errors.error', code=403, msg=print_exc()))
+            return redirect(url_for("index", msg="You must be signed in to do this"))
         else:
             return json.dumps({
                 "code": 403,
@@ -82,14 +92,23 @@ def addMember():
 
 @groupsBP.route("/removeMember", methods=["POST"])
 def removeMember():
-    isBrowser = "userid" in request.form
+    """ Endpoint to remove a user from a specific group - same endpoint to allow user to leave a group
+
+        - groupid: the UUID of the group in which the file will become associated with
+        - userid (cookie): the UUID of the user who triggered the member removal, in a cookie
+        - userid: UUID of the user who should be removed from the group, in the form data
+
+        - returns: a response for the client to interpret based on the success of the operation
+    """
+
+    isBrowser = "userid" in request.form    # Checks if the request is coming from the browser or the CLI
     data = request.form if isBrowser else json.loads(request.data)
 
     if request.cookies.get("userid"):
-        try:
-            print(data)
+        try:                            # Fetch the group data from the provided groupid
             group = GroupTable.query.filter_by(groupid=data["groupid"]).first()
 
+            # Checks the group exists, and that either the user is the leader OR they are attempting to remove themselves (i.e: leaving the group)
             if not group or not (str(group.groupleaderid) is not request.cookies.get("userid") and request.cookies.get("userid") is not data["userid"]):
                 if isBrowser:
                     return redirect(url_for('dash', msg="Sorry you are not permitted to complete this action"))
@@ -99,11 +118,12 @@ def removeMember():
                         "msg": "You don't have permission to complete this action!"
                     }), 401
 
-
+            # Finds the record associated with the user in the UserGroupTable
             usergroup = UserGroupTable.query.filter(and_(UserGroupTable.userid==data["userid"], UserGroupTable.groupid==data["groupid"])).first()
-            db.session.delete(usergroup)
+            db.session.delete(usergroup)        # Deletes the record
             db.session.commit()
 
+            # Different responses depending on if the user has removed themselves, or another user
             if data["userid"] == request.cookies.get("userid"):
                 if isBrowser:
                     return redirect(url_for('dash', msg="You have left " + group.groupname))
@@ -122,10 +142,9 @@ def removeMember():
                     })
         except Exception as e:
             print(print_exc())
-            return str(e)
 
             if isBrowser:
-                return redirect(url_for('errors.error', code=500, msg=print_exc()))
+                return redirect(url_for("group", id=data["groupid"], msg="Something went wrong when removing user"))
             else:
                 return json.dumps({
                     "code": 500,
@@ -133,41 +152,47 @@ def removeMember():
                 }), 500
     else:
         if isBrowser:
-            return redirect(url_for('errors.error', code=403, msg=print_exc()))
+            return redirect(url_for("index", msg="You must be signed in to do this"))
         else:
             return json.dumps({
                 "code": 403,
                 "msg": "You must be signed in to do this",
-                "exc": print_exc()
             }), 403
 
 
 @groupsBP.route("/new", methods=["POST"])
 def newGroup():
-    isBrowser = "groupname" in request.form
+    """ Endpoint to create a new group
+
+        - groupname: the user-inputted name of the group to be created
+        - userid: UUID of the user creating the group, so they can be set to the groupleader
+
+        - returns: a response for the client to interpret based on the success of the operation
+    """
+
+    isBrowser = "groupname" in request.form                 # Check for which platform request is coming from
     data = request.form if isBrowser else json.loads(request.data)
 
     if request.cookies.get("userid"):
         try:
-            group = GroupTable({
+            group = GroupTable({                            # Creates a record in the GroupTable with the provided data
                 "groupname": data.get("groupname"),
                 "groupleaderid": request.cookies.get("userid")
             })
-            db.session.add(group)
+            db.session.add(group)                           # Adds the group to allow foreign key constraints
             db.session.commit()
 
-            usergroup = UserGroupTable({
+            usergroup = UserGroupTable({                    # Creates a UserGroup record for the current user
                 "groupid": str(group.groupid),
                 "userid": request.cookies.get("userid")
             })
             db.session.add(usergroup)
             db.session.commit()
         except Exception as e:
-            print(print_exc())
-            return str(e)
+            print(print_exc())                              #
 
             if isBrowser:
-                return redirect(url_for('errors.error', code=500, msg=print_exc()))
+                return redirect(url_for("dash", msg="Something went wrong when creating your group"))
             else:
                 return json.dumps({
                     "code": 500,
@@ -175,7 +200,7 @@ def newGroup():
                 }), 500
     else:
         if isBrowser:
-            return redirect(url_for('errors.error', code=403, msg=print_exc()))
+            return redirect(url_for("index", msg="You must be signed in to do this"))
         else:
             return json.dumps({
                 "code": 403,
@@ -184,7 +209,7 @@ def newGroup():
             }), 403
 
     if isBrowser:
-        return redirect(url_for('group', id=group.groupid))
+        return redirect(url_for("group", id=group.groupid))
     else:
         return json.dumps({
             "code": 200,
@@ -194,6 +219,15 @@ def newGroup():
 
 @groupsBP.route("/rename", methods=["POST"])
 def renameGroup():
+    """ Endpoint to rename a specified group
+
+        - groupid: the UUID of the group to rename
+        - userid (cookie): the UUID of the user who triggered the rename, and to check they have correct permissions
+        - newname: string of the name to which the specified group should be changed to
+
+        - returns: a response for the client to interpret based on the success of the operation
+    """
+
     isBrowser = "groupid" in request.form
     data = request.form if isBrowser else request.data
 
@@ -216,23 +250,20 @@ def renameGroup():
             db.session.commit()
 
         except Exception as e:
-            return str(e)
-
             if isBrowser:
-                return redirect(url_for('errors.error', code=500, msg=print_exc()))
+                return redirect(url_for("group", id=data["groupid"], msg="Something went wrong when renaming your group"))
             else:
                 return json.dumps({
                     "code": 500,
-                    "msg": "Something went wrong when creating your group"
+                    "msg": "Something went wrong when renaming your group"
                 }), 500
     else:
         if isBrowser:
-            return redirect(url_for('errors.error', code=403, msg=print_exc()))
+            return redirect(url_for("index", msg="You must be signed in to do this"))
         else:
             return json.dumps({
                 "code": 403,
                 "msg": "You must be signed in to do this",
-                "exc": print_exc()
             }), 403
 
     if isBrowser:
@@ -246,6 +277,14 @@ def renameGroup():
 
 @groupsBP.route("/delete", methods=["POST"])
 def deleteGroup():
+    """ Endpoint to delete a group, and all data associated with it
+
+        - groupid: the UUID of the group to delete
+        - userid: the UUID of the user who triggered the deletion, to check permissions
+
+        - returns: a response for the client to interpret based on the success of the operation
+    """
+
     isBrowser = "groupid" in request.form
     data = request.form if isBrowser else request.data
 
@@ -254,7 +293,8 @@ def deleteGroup():
             group = GroupTable.query.filter(and_(GroupTable.groupid==data["groupid"],
                                                  GroupTable.groupleaderid==request.cookies.get("userid"))).first()
 
-            if not group and not getUserData(request.cookies.get("userid")).admin:
+            # Checks whether user is a groupleader or if they are an admin
+            if not group and not getUserData(request.cookies.get("userid"))["admin"]:
                 if isBrowser:
                     return redirect(url_for('dash', msg="Sorry you are not permitted to complete this action"))
                 else:
@@ -263,16 +303,15 @@ def deleteGroup():
                         "msg": "You don't have permission to complete this action!"
                     }), 401
 
-            db.session.delete(group)
+            db.session.delete(group)    # Delete record and commit the changes
             db.session.commit()
 
             return redirect(url_for('dash', msg=group.groupname + " has been deleted"))
         except Exception as e:
             print(print_exc())
-            return str(e)
 
             if isBrowser:
-                return redirect(url_for('errors.error', code=500, msg=print_exc()))
+                return redirect(url_for("group", id=data["groupid"], msg="Something went wrong when deleting your group"))
             else:
                 return json.dumps({
                     "code": 500,
@@ -280,53 +319,7 @@ def deleteGroup():
                 }), 500
     else:
         if isBrowser:
-            return redirect(url_for('errors.error', code=403, msg=print_exc()))
-        else:
-            return json.dumps({
-                "code": 403,
-                "msg": "You must be signed in to do this",
-                "exc": print_exc()
-            }), 403
-
-
-@groupsBP.route("/getGroups", methods=["GET"])
-def getGroups():
-    """ Returns the groups that the user is a part of
-    """
-    isBrowser = "groupid" in request.form
-    # data = request.form if isBrowser else json.loads(request.data)
-
-    if request.cookies.get("userid"):
-        userid = request.cookies.get('userid')
-        try:
-            # Query all groups that the user belongs to in psql
-            groups = getUserGroups(userid)
-
-            rs = []
-            print('\n\nGetting groups for user: ' + userid + ' query...')
-            for g in range(len(groups)):
-                group = groups[g]
-                print('Group', str(g + 1) + ':', group)
-                rs.append(group)
-
-            resp = make_response(json.dumps({"code": 200, "msg": "Here are the groups you are a member of", "rows": rs}))
-            resp.set_cookie("userid", userid)
-
-            return resp
-        except Exception as e:
-            print(print_exc())
-            return str(e)
-
-            if isBrowser:
-                return redirect(url_for('errors.error', code=500, msg=print_exc()))
-            else:
-                return json.dumps({
-                    "code": 500,
-                    "msg": "Something went wrong when querying the groups."
-                }), 500
-    else:
-        if isBrowser:
-            return redirect(url_for('errors.error', code=403, msg=print_exc()))
+            return redirect(url_for("index", msg="You need to be signed in to do this"))
         else:
             return json.dumps({
                 "code": 403,
--- src/api/groups/utils.py
+++ src/api/groups/utils.py
@@ -19,33 +19,38 @@ def getUserGroups(userID):
         return []       # If an
 
 
-def isGroupLeader(userID, groupID):
-    try:
-        res = GroupTable.query.filter(and_(GroupTable.groupleaderid == userID, GroupTable.groupid == groupID))
-        return res.count() > 0
-    except Exception as e:
-        print(print_exc())
-        return False
-
-
 def getGroupUsers(groupID):
+    """ Gets the user data of all the members of a group
+
+        - groupid: the UUID of the group to get members of
+
+        - returns: a response for the client to interpret based on the success of the operation
+    """
+
     try:
         res = UserTable.query.join(UserGroupTable, and_(UserGroupTable.userid == UserTable.userid,
-                                                        UserGroupTable.groupid == groupID)).all()
-        return list(map(lambda user: getUserData(user.userid), res))
+                                                        UserGroupTable.groupid == groupID)).all()   # Query to fetch all
+        return list(map(lambda user: getUserData(user.userid), res))                                # members from group
     except Exception as e:
         print(print_exc())
-        return []
+        return []           # Returns an empty list if there is an exception
 
 
 def getGroupData(groupID):
+    """ Endpoint to remove a user from a specific group - same endpoint to allow user to leave a group
+
+        - groupid: returns the serialised JSON data from a group, alongside all the files they can access
+
+        - returns: a response for the client to interpret based on the success of the operation
+    """
+
     groupData = GroupTable.query.filter_by(groupid=groupID).first()
 
     return {
         "groupid": groupID,
         "members": getGroupUsers(groupID),
-        "files": src.api.files.file_query.file_query({"groupid": groupID}),
-        "groupname": groupData.groupname,
+        "files": src.api.files.file_query.file_query({"groupid": groupID}),  # Has to do absolute path of the module here
+        "groupname": groupData.groupname,                                    # due to circular import error
         "groupleader": getUserData(groupData.groupleaderid),
 
     }
